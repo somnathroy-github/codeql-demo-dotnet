9ff3e2e47ef5988c31ce19f958afeb34be48494d
Somnath Roy <Somnath.Roy@hyland.com>
2024-05-27 15:15:07 +0530

    workflow modified

--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13b9a1fddcc9eb2a8f59b4306467ebc41386797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1fa3ba8a1029f7388ac2d2d70ddb06a4f19f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
