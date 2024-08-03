f3e492c474598830407d6bd917c3c124ac37257a
somnathroy-github <159121780+somnathroy-github@users.noreply.github.com>
2024-08-03 09:16:21 +0530

    Update Calculator.cs

--- MyConsoleApp/Calculator.cs
+++ MyConsoleApp/Calculator.cs
@@ -9,7 +9,7 @@
 
         public int Subtract(int a, int b)
         {
-            return a - b;
+            return a - b; //check PR template
         }
 
         public int Multiply(int a, int b)
